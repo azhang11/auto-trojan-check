7bf8521315c3f18fbf73c646c1c51c78ef0ff3a8
azhang11 <andrew.zhang883@gmail.com>
2021-11-21 22:18:49 -0800

    Logs in and clicks continue

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
-# C:\Users\andre\Downloads\chromedriver_win32 (1)
-# https://www.stealmylogin.com/demo.html
+# Used to import the webdriver from selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options
-import requests
+import time
+import os
 
-url = "https://www.goldmansachs.com/"
-username = "tomsmith"
-password = "SuperSecretPassword!"
 
-#options = webdriver.ChromeOptions()
-#options.add_experimental_option("excludeSwitches", ["enable-automation"])
-#options.add_experimental_option("useAutomationExtension", False)
+# Get the path of chromedriver which you have install
 
-service = ChromeService(executable_path="C:\\Dev\\WebDrivers\\chromedriver.exe")
-driver = webdriver.Chrome(service=service)
-driver.get(url)
-while (True):
-    pass
-print(driver.title)
+def startBot(username, password, url):
+    path = "C:\\Dev\\WebDrivers\\chromedriver.exe"
 
-#driver.find_element("username").send_keys(username)
-#driver.find_element("password").send_keys(password)
-#login_button = driver.find_element("submit")
-#login_button.submit()
+    # giving the path of chromedriver to selenium webdriver
+    driver = webdriver.Chrome(path)
+
+    # opening the website in chrome.
+    driver.get(url)
+    # driver.implicitly_wait(20000)
+    time.sleep(2)
+
+    driver.find_element_by_class_name("button-wrapper").click()
+    time.sleep(3)
+    # find the id or name or class of
+    # username by inspecting on username input
+    driver.find_element_by_name("j_username").send_keys(username)
+
+    # find the password by inspecting on password input
+    driver.find_element_by_name("j_password").send_keys(password)
+
+    # click on submit
+    # driver.find_element_by_css_selector("_eventId_proceed").click()
+    driver.find_element_by_name("_eventId_proceed").click()
+    time.sleep(3)
+    driver.find_element_by_class_name("mat-focus-indicator.submit-button.btn-next.mat-button.mat-button-base.mat-accent").click()
+    while (True):
+      pass
+
+# Driver Code
+# Enter below your login credentials
+username = "azhang65"
+password = "#Vcs6924@Vcs6924"
+
+# URL of the login page of site
+# which you want to automate login.
+url = "https://trojancheck.usc.edu/login"
+# Call the function
+startBot(username, password, url)

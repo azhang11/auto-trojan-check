61a16eee4e8e28990403baabc0fa56a655e11aae
azhang11 <andrew.zhang883@gmail.com>
2021-11-21 23:19:31 -0800

    Finishes the entire trojan check, just needs to send the email now

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 # Get the path of chromedriver which you have install
 
 def startBot(username, password, url):
+
     path = "C:\\Dev\\WebDrivers\\chromedriver.exe"
 
     # giving the path of chromedriver to selenium webdriver
@@ -26,18 +27,44 @@ def startBot(username, password, url):
     # find the password by inspecting on password input
     driver.find_element_by_name("j_password").send_keys(password)
 
-    # click on submit
-    # driver.find_element_by_css_selector("_eventId_proceed").click()
+    #Process to start assessment
     driver.find_element_by_name("_eventId_proceed").click()
     time.sleep(3)
     driver.find_element_by_class_name("mat-focus-indicator.submit-button.btn-next.mat-button.mat-button-base.mat-accent").click()
+    time.sleep(3)
+    driver.find_element_by_class_name("mat-focus-indicator.mat-flat-button.mat-button-base.btn-begin-assessment").click()
+    time.sleep(3)
+    driver.find_element_by_class_name("mat-focus-indicator.btn-assessment-start.mat-flat-button.mat-button-base").click()
+    time.sleep(3)
+
+    #No and no questions, then next
+    driver.find_element_by_id("mat-button-toggle-3").click()
+    driver.find_element_by_id("mat-button-toggle-5").click()
+    driver.find_element_by_class_name("mat-focus-indicator.btn-next.mat-flat-button.mat-button-base").click()
+
+    #Last page of No's
+    time.sleep(3)
+    driver.find_element_by_id("mat-button-toggle-14").click()
+    driver.find_element_by_id("mat-button-toggle-16").click()
+    driver.find_element_by_id("mat-button-toggle-18").click()
+    driver.find_element_by_id("mat-button-toggle-20").click()
+    driver.find_element_by_id("mat-button-toggle-22").click()
+    driver.find_element_by_id("mat-button-toggle-24").click()
+    driver.find_element_by_id("mat-button-toggle-26").click()
+    driver.find_element_by_class_name("mat-focus-indicator.btn-next.mat-flat-button.mat-button-base").click()
+
+    #Verify responses
+    time.sleep(3)
+    driver.find_element_by_class_name("mat-checkbox-inner-container").click()
+    driver.find_element_by_class_name("mat-focus-indicator.btn-submit.mat-flat-button.mat-button-base").click()
     while (True):
       pass
 
 # Driver Code
 # Enter below your login credentials
-username = "azhang65"
-password = "#Vcs6924@Vcs6924"
+f = open("logininfo.txt", "r")
+username = f.readline()
+password = f.readline()
 
 # URL of the login page of site
 # which you want to automate login.
